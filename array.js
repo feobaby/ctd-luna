0ac587a86f32b048ccf3ca0a3230a1a5de62afc8
feobaby <olufunmilayo335@gmail.com>
2025-08-11 20:14:07 -0500

    add more companies

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 // An array of favourite companies!
 
-const funmisArray = ["CTD", "ANDELA", "GOOGLE", "SAGO MINI", "NETFLIX"]
+const funmisArray = ["CTD", "ANDELA", "GOOGLE", "SAGO MINI", "NETFLIX", "META", "YOUTUBE"]
 
 console.log(funmisArray)
